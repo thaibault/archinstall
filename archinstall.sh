b9dc32f3afa2a0f281c76699e3a75d7cd2ddcbb5
torben <t.sickert[~at~]gmail.com>
2020-12-23 22:22:06 +0100

    Automatic update to new version.

--- archinstall.sh
+++ archinstall.sh
@@ -756,13 +756,13 @@ ai_configure() {
         bl.logging.info \
             Configure initramfs to support decrypting root system at boot.
         # TODO test
-        #'s/^(HOOKS=\().+(\))$/\1base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt filesystems fsck\2/' \
+        #'s/^(HOOKS=\().+(\))$/\1base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt filesystems\2/' \
 
         #'s/^(HOOKS=\().+(\))$/\1autodetect block keyboard sd-encrypt sd-vconsole systemd\2/' \
         sed \
             --in-place \
             --regexp-extended \
-            's/^(HOOKS=\().+(\))$/\1autodetect block filesystems keyboard modconf sd-encrypt sd-vconsole systemd\2/' \
+            's/^(HOOKS=\().+(\))$/\1base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt filesystems\2/' \
             "${ai_mountpoint_path}etc/mkinitcpio.conf"
     fi
     bl.logging.info \
